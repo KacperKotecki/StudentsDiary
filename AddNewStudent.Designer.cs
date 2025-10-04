d6c194a727a651e1f7cc40ea4e7d7f914d44fa14
Kacper Kotecki <kacperkotecki@o2.pl>
2025-10-04 12:52:41 +0200

    Add fields: Profile name, Academic degree, Year, Group, Specialization to the AddNewStudent form

--- AddNewStudent.Designer.cs
+++ AddNewStudent.Designer.cs
@@ -42,103 +42,127 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.lbProfileName = new System.Windows.Forms.Label();
+            this.lbDegree = new System.Windows.Forms.Label();
+            this.lbYear = new System.Windows.Forms.Label();
+            this.lbGroup = new System.Windows.Forms.Label();
+            this.cbProfileName = new System.Windows.Forms.ComboBox();
+            this.cbDegree = new System.Windows.Forms.ComboBox();
+            this.nudYear = new System.Windows.Forms.NumericUpDown();
+            this.cbSpecjalization = new System.Windows.Forms.ComboBox();
+            this.lbSpecjalization = new System.Windows.Forms.Label();
+            this.chbSpecjalization = new System.Windows.Forms.CheckBox();
+            this.cbGroups = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).BeginInit();
             this.SuspendLayout();
             // 
             // lbId
             // 
             this.lbId.AutoSize = true;
-            this.lbId.Location = new System.Drawing.Point(12, 19);
+            this.lbId.Location = new System.Drawing.Point(16, 23);
+            this.lbId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbId.Name = "lbId";
-            this.lbId.Size = new System.Drawing.Size(21, 13);
+            this.lbId.Size = new System.Drawing.Size(23, 16);
             this.lbId.TabIndex = 0;
             this.lbId.Text = "ID:";
             // 
             // tbId
             // 
-            this.tbId.Location = new System.Drawing.Point(99, 16);
+            this.tbId.Location = new System.Drawing.Point(132, 20);
+            this.tbId.Margin = new System.Windows.Forms.Padding(4);
             this.tbId.Name = "tbId";
             this.tbId.ReadOnly = true;
-            this.tbId.Size = new System.Drawing.Size(200, 20);
+            this.tbId.Size = new System.Drawing.Size(72, 22);
             this.tbId.TabIndex = 1;
             // 
             // tbFirstName
             // 
-            this.tbFirstName.Location = new System.Drawing.Point(99, 48);
+            this.tbFirstName.Location = new System.Drawing.Point(132, 59);
+            this.tbFirstName.Margin = new System.Windows.Forms.Padding(4);
             this.tbFirstName.Name = "tbFirstName";
-            this.tbFirstName.Size = new System.Drawing.Size(200, 20);
+            this.tbFirstName.Size = new System.Drawing.Size(222, 22);
             this.tbFirstName.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.Size = new System.Drawing.Size(32, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Imię";
             // 
             // tbPesel
             // 
-            this.tbPesel.Location = new System.Drawing.Point(99, 117);
+            this.tbPesel.Location = new System.Drawing.Point(132, 159);
+            this.tbPesel.Margin = new System.Windows.Forms.Padding(4);
             this.tbPesel.Name = "tbPesel";
-            this.tbPesel.Size = new System.Drawing.Size(200, 20);
+            this.tbPesel.Size = new System.Drawing.Size(222, 22);
             this.tbPesel.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Location = new System.Drawing.Point(16, 163);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.Size = new System.Drawing.Size(42, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Pesel";
             // 
             // tbLastName
             // 
-            this.tbLastName.Location = new System.Drawing.Point(99, 85);
+            this.tbLastName.Location = new System.Drawing.Point(132, 105);
+            this.tbLastName.Margin = new System.Windows.Forms.Padding(4);
             this.tbLastName.Name = "tbLastName";
-            this.tbLastName.Size = new System.Drawing.Size(200, 20);
+            this.tbLastName.Size = new System.Drawing.Size(222, 22);
             this.tbLastName.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 88);
+            this.label4.Location = new System.Drawing.Point(16, 108);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.Size = new System.Drawing.Size(65, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "Nazwisko";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 187);
+            this.label5.Location = new System.Drawing.Point(16, 269);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.Size = new System.Drawing.Size(101, 16);
             this.label5.TabIndex = 10;
             this.label5.Text = "Data Urodzenia";
             // 
             // tbIndexNumber
             // 
-            this.tbIndexNumber.Location = new System.Drawing.Point(99, 152);
+            this.tbIndexNumber.Location = new System.Drawing.Point(132, 213);
+            this.tbIndexNumber.Margin = new System.Windows.Forms.Padding(4);
             this.tbIndexNumber.Name = "tbIndexNumber";
-            this.tbIndexNumber.Size = new System.Drawing.Size(200, 20);
+            this.tbIndexNumber.Size = new System.Drawing.Size(222, 22);
             this.tbIndexNumber.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 155);
+            this.label6.Location = new System.Drawing.Point(16, 217);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.Size = new System.Drawing.Size(71, 16);
             this.label6.TabIndex = 8;
             this.label6.Text = "Nr Indeksu";
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(12, 224);
+            this.btnSave.Location = new System.Drawing.Point(426, 346);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(140, 23);
+            this.btnSave.Size = new System.Drawing.Size(157, 40);
             this.btnSave.TabIndex = 12;
             this.btnSave.Text = "Zapisz";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -146,9 +170,10 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(159, 224);
+            this.btnCancel.Location = new System.Drawing.Point(602, 346);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(140, 23);
+            this.btnCancel.Size = new System.Drawing.Size(157, 40);
             this.btnCancel.TabIndex = 13;
             this.btnCancel.Text = "Anuluj";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -156,16 +181,127 @@
             // 
             // dtpBirthday
             // 
-            this.dtpBirthday.Location = new System.Drawing.Point(99, 187);
+            this.dtpBirthday.Location = new System.Drawing.Point(132, 269);
+            this.dtpBirthday.Margin = new System.Windows.Forms.Padding(4);
             this.dtpBirthday.Name = "dtpBirthday";
-            this.dtpBirthday.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirthday.Size = new System.Drawing.Size(222, 22);
             this.dtpBirthday.TabIndex = 14;
+            // 
+            // lbProfileName
+            // 
+            this.lbProfileName.AutoSize = true;
+            this.lbProfileName.Location = new System.Drawing.Point(382, 18);
+            this.lbProfileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbProfileName.Name = "lbProfileName";
+            this.lbProfileName.Size = new System.Drawing.Size(107, 16);
+            this.lbProfileName.TabIndex = 15;
+            this.lbProfileName.Text = "Kierunek studiów";
+            // 
+            // lbDegree
+            // 
+            this.lbDegree.AutoSize = true;
+            this.lbDegree.Location = new System.Drawing.Point(382, 60);
+            this.lbDegree.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbDegree.Name = "lbDegree";
+            this.lbDegree.Size = new System.Drawing.Size(45, 16);
+            this.lbDegree.TabIndex = 17;
+            this.lbDegree.Text = "Studia";
+            // 
+            // lbYear
+            // 
+            this.lbYear.AutoSize = true;
+            this.lbYear.Location = new System.Drawing.Point(382, 102);
+            this.lbYear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbYear.Name = "lbYear";
+            this.lbYear.Size = new System.Drawing.Size(32, 16);
+            this.lbYear.TabIndex = 18;
+            this.lbYear.Text = "Rok";
+            // 
+            // lbGroup
+            // 
+            this.lbGroup.AutoSize = true;
+            this.lbGroup.Location = new System.Drawing.Point(382, 159);
+            this.lbGroup.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbGroup.Name = "lbGroup";
+            this.lbGroup.Size = new System.Drawing.Size(44, 16);
+            this.lbGroup.TabIndex = 19;
+            this.lbGroup.Text = "Grupa";
+            // 
+            // cbProfileName
+            // 
+            this.cbProfileName.FormattingEnabled = true;
+            this.cbProfileName.Location = new System.Drawing.Point(496, 15);
+            this.cbProfileName.Name = "cbProfileName";
+            this.cbProfileName.Size = new System.Drawing.Size(222, 24);
+            this.cbProfileName.TabIndex = 20;
+            // 
+            // cbDegree
+            // 
+            this.cbDegree.FormattingEnabled = true;
+            this.cbDegree.Location = new System.Drawing.Point(496, 55);
+            this.cbDegree.Name = "cbDegree";
+            this.cbDegree.Size = new System.Drawing.Size(222, 24);
+            this.cbDegree.TabIndex = 21;
+            // 
+            // nudYear
+            // 
+            this.nudYear.Location = new System.Drawing.Point(496, 102);
+            this.nudYear.Name = "nudYear";
+            this.nudYear.Size = new System.Drawing.Size(222, 22);
+            this.nudYear.TabIndex = 23;
+            // 
+            // cbSpecjalization
+            // 
+            this.cbSpecjalization.Enabled = false;
+            this.cbSpecjalization.FormattingEnabled = true;
+            this.cbSpecjalization.Location = new System.Drawing.Point(524, 211);
+            this.cbSpecjalization.Name = "cbSpecjalization";
+            this.cbSpecjalization.Size = new System.Drawing.Size(194, 24);
+            this.cbSpecjalization.TabIndex = 25;
+            // 
+            // lbSpecjalization
+            // 
+            this.lbSpecjalization.AutoSize = true;
+            this.lbSpecjalization.Location = new System.Drawing.Point(382, 214);
+            this.lbSpecjalization.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbSpecjalization.Name = "lbSpecjalization";
+            this.lbSpecjalization.Size = new System.Drawing.Size(88, 16);
+            this.lbSpecjalization.TabIndex = 24;
+            this.lbSpecjalization.Text = "Specjalizacja";
+            // 
+            // chbSpecjalization
+            // 
+            this.chbSpecjalization.AutoSize = true;
+            this.chbSpecjalization.Location = new System.Drawing.Point(496, 213);
+            this.chbSpecjalization.Name = "chbSpecjalization";
+            this.chbSpecjalization.Size = new System.Drawing.Size(18, 17);
+            this.chbSpecjalization.TabIndex = 26;
+            this.chbSpecjalization.UseVisualStyleBackColor = true;
+            // 
+            // cbGroups
+            // 
+            this.cbGroups.FormattingEnabled = true;
+            this.cbGroups.Location = new System.Drawing.Point(496, 151);
+            this.cbGroups.Name = "cbGroups";
+            this.cbGroups.Size = new System.Drawing.Size(222, 24);
+            this.cbGroups.TabIndex = 27;
             // 
             // AddNewStudent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 269);
+            this.ClientSize = new System.Drawing.Size(772, 399);
+            this.Controls.Add(this.cbGroups);
+            this.Controls.Add(this.chbSpecjalization);
+            this.Controls.Add(this.cbSpecjalization);
+            this.Controls.Add(this.lbSpecjalization);
+            this.Controls.Add(this.nudYear);
+            this.Controls.Add(this.cbDegree);
+            this.Controls.Add(this.cbProfileName);
+            this.Controls.Add(this.lbGroup);
+            this.Controls.Add(this.lbYear);
+            this.Controls.Add(this.lbDegree);
+            this.Controls.Add(this.lbProfileName);
             this.Controls.Add(this.dtpBirthday);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
@@ -180,11 +316,13 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tbId);
             this.Controls.Add(this.lbId);
-            this.MaximumSize = new System.Drawing.Size(342, 308);
-            this.MinimumSize = new System.Drawing.Size(342, 308);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(790, 446);
+            this.MinimumSize = new System.Drawing.Size(790, 446);
             this.Name = "AddNewStudent";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dodaj Studenta";
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,5 +344,16 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.Label lbProfileName;
+        private System.Windows.Forms.Label lbDegree;
+        private System.Windows.Forms.Label lbYear;
+        private System.Windows.Forms.Label lbGroup;
+        private System.Windows.Forms.ComboBox cbProfileName;
+        private System.Windows.Forms.ComboBox cbDegree;
+        private System.Windows.Forms.NumericUpDown nudYear;
+        private System.Windows.Forms.ComboBox cbSpecjalization;
+        private System.Windows.Forms.Label lbSpecjalization;
+        private System.Windows.Forms.CheckBox chbSpecjalization;
+        private System.Windows.Forms.ComboBox cbGroups;
     }
 }
